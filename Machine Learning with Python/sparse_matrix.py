5a220191758fa14a7b104e22aa049b4fd976f561
roshangrewal <roshangrewal@hotmail.com>
2019-08-06 01:11:44 +0530

    ML

--- Machine Learning with Python/sparse_matrix.py
+++
@@ -1,9 +0,0 @@
-from scipy.sparse import *
-from scipy import *
-from numpy import *
-import csv
-S = dok_matrix((10000,10000), dtype=bool)
-f = open("sampledata.csv")
-reader = csv.reader(f)
-for line in reader:
-    S[int(line[0]),int(line[1])] = True
